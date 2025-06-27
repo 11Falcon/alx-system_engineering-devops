6e4cd0ff48d60db969cc04e241e36cb9c3e22cfb
Soufiane <soufin9mkns@gmail.com>
2025-06-27 05:33:19 -0700

    pip2 install Flask==2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,6 +5,6 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
