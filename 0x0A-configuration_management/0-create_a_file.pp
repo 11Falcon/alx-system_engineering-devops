507a8b1b12d101ed033f427f8648c1dc2b3ef4e8
Soufiane <soufin9mkns@gmail.com>
2025-06-27 02:21:46 -0700

    first puppet : with puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 # first puppet file : creating a file named school in the tmp directory with a content of "I love Puppet"
 file { 'create a file' :
-	path => "/tmp/school",
-	mode => '0744',
-	owner =>  "www-data",
-	group => "www-data"
-	content => "I love Puppet",
+  path  => '/tmp/school',
+  mode  => '0744',
+  owner => 'www-data',
+  group => 'www-data',
 }
