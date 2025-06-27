0f061801b6f83caec05c4efc978598a3794f8cb4
Soufiane <soufin9mkns@gmail.com>
2025-06-27 05:04:56 -0700

    pip instal Flask==2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,11 @@
-# Ensure pip3 is available (optional but recommended)
+# Ensure pip3 is installed
+
 package { 'python3-pip':
   ensure => installed,
 }
 
-
-# Puppet for installing flask package
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+# Install Flask version 2.1.0 via pip3
+exec { 'install_flask':
+ command => '/usr/bin/pip3 install Flask==2.1.0',
+ unless  => '/usr/bin/pip3 show flask | grep -q "Version:2.1.0"',
 }
